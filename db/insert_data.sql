2ba563703d4333078a474bab79abd32a1906efba
Marco Fracassi <marco.fracassi@motork.io>
2023-09-11 09:02:32 +0200

    Fix initial data

--- db/insert_data.sql
+++ db/insert_data.sql
@@ -1,6 +1,6 @@
 \c shop
 
-INSERT INTO products (name, description, image) VALUES
+INSERT INTO products (name, description, image, available_quantity, booked_quantity) VALUES
 ('Daikon', 'The best one!', 'https://www.highmowingseeds.com/media/catalog/product/cache/6cbdb003cf4aae33b9be8e6a6cf3d7ad/2/8/2862.jpg', 100, 0),
 ('Garlic', 'I like it', 'https://i.guim.co.uk/img/media/43442656c722147e053264b6b914b3258642db2e/0_37_3812_2290/master/3812.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=24943c703401dec3d0e8e0da56fec7aa', 43, 12),
 ('Onion', 'So good', 'https://cdn.vox-cdn.com/thumbor/LJ1_MNs7FuK955wwXbWY8hiEwtw=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/21769456/shutterstock_678310408.jpg', 77, 32),
